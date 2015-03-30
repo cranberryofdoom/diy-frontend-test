ec5aa74304e4de5d7f9f7fb51918de4466c64f52
Ashley Qian <ashley.qian@duke.edu>
2015-03-29 21:27:58 -0400

    Finished Jade templating and styling for render. Added a request to retrieve data for the current hard-coded user who will be able to comment.

--- server/api/project/project.controller.js
+++ server/api/project/project.controller.js
@@ -4,7 +4,39 @@ var _ = require('lodash');
 var request = require('request');
 var Q = require('q');
 var moment = require('moment');
+var humanizeDuration = require("humanize-duration");
 
+var shortEnglishHumanizer = humanizeDuration.humanizer({
+  language: "shortEn",
+  languages: {
+    shortEn: {
+      year: function() {
+        return "y";
+      },
+      month: function() {
+        return "mo";
+      },
+      week: function() {
+        return "w";
+      },
+      day: function() {
+        return "d";
+      },
+      hour: function() {
+        return "h";
+      },
+      minute: function() {
+        return "m";
+      },
+      second: function() {
+        return "s";
+      },
+      millisecond: function() {
+        return "ms";
+      },
+    }
+  }
+});
 
 exports.show = function(req, res) {
 
@@ -15,8 +47,9 @@ exports.show = function(req, res) {
     var result = Q.defer();
     requests.push(result.promise);
     request(projectDetailURL, function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var details = JSON.parse(body).response;
+      if (error) {
+        result.reject(new Error(error));
+      } else if (response.statusCode == 200) {
         result.resolve(JSON.parse(body).response);
       }
     });
@@ -26,7 +59,9 @@ exports.show = function(req, res) {
     var result = Q.defer();
     requests.push(result.promise);
     request(projectDetailURL + "/comments", function(error, response, body) {
-      if (!error && response.statusCode == 200) {
+      if (error) {
+        result.reject(new Error(error));
+      } else if (response.statusCode == 200) {
         result.resolve(JSON.parse(body).response);
       }
     });
@@ -36,7 +71,21 @@ exports.show = function(req, res) {
     var result = Q.defer();
     requests.push(result.promise);
     request(projectDetailURL + "/favorites", function(error, response, body) {
-      if (!error && response.statusCode == 200) {
+      if (error) {
+        result.reject(new Error(error));
+      } else if (response.statusCode == 200) {
+        result.resolve(JSON.parse(body).response);
+      }
+    });
+  }
+
+  function getCurrentMakerDetails() {
+    var result = Q.defer();
+    requests.push(result.promise);
+    request("http://api.diy.org/makers/hackkitty", function(error, response, body) {
+      if (error) {
+        result.reject(new Error(error));
+      } else if (response.statusCode == 200) {
         result.resolve(JSON.parse(body).response);
       }
     });
@@ -45,13 +94,20 @@ exports.show = function(req, res) {
   getProjectDetails();
   getProjectComments();
   getProjectFavorites();
+  getCurrentMakerDetails();
 
   Q.all(requests).then(function(results) {
+    var date = moment(new Date(results[0].stamp));
+    for (var i = 0; i < results[1].length; i++) {
+      var dateDiff = shortEnglishHumanizer(new Date() - new Date(results[1][i].stamp));
+      requests[1][i].stamp = dateDiff.split(",")[0];
+    }
     res.render("project/show", {
-      date: moment(results[0].stamp).format("MMM D, YYYY"),
+      date: date.format("MMM D, YYYY"),
       details: results[0],
       comments: results[1],
-      favorites: results[2]
+      favorites: results[2],
+      currentMaker: results[3]
     });
   }, console.error);
 };
